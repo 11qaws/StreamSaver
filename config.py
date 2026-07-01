fd747cf47aef6495b295ab8e1a25d61179dc37e4
11qaws <11qaws@users.noreply.github.com>
2026-07-01 09:43:58 +0900

    v1.1.13: HTML 이스케이프 처리로 XSS 방어 강화

--- config.py
+++ config.py
@@ -99,7 +99,7 @@ except ImportError:
 
 NODE_JS = shutil.which('node') or ''
 
-APP_VERSION    = "1.1.12"
+APP_VERSION    = "1.1.13"
 GITHUB_REPO    = "11qaws/StreamSaver"
 BOT_INVITE_URL = (
     "https://discord.com/oauth2/authorize"
--- installer/setup.iss
+++ installer/setup.iss
@@ -1,5 +1,5 @@
 #define MyAppName "StreamSaver"
-#define MyAppVersion "1.1.12"
+#define MyAppVersion "1.1.13"
 #define MyAppPublisher "StreamSaver"
 #define MyAppExeName "StreamSaver.exe"
 #define RelayURL "ws://217.142.229.237:8765"
