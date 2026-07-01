c3261fe3333b9f11ab612507f862c0d27b38ea12
11qaws <11qaws@users.noreply.github.com>
2026-07-01 22:09:12 +0900

    v1.1.25: URL 일괄 추가 모달 (+ 여러 개 버튼)

--- config.py
+++ config.py
@@ -99,7 +99,7 @@ except ImportError:
 
 NODE_JS = shutil.which('node') or ''
 
-APP_VERSION    = "1.1.24"
+APP_VERSION    = "1.1.25"
 GITHUB_REPO    = "11qaws/StreamSaver"
 BOT_INVITE_URL = (
     "https://discord.com/oauth2/authorize"
